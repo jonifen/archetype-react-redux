faebcf38aa4f70e2e07e2d64c7968f4a2bc11b13
jonifen <jonifen@users.noreply.github.com>
2021-03-13 22:03:17 +0000

    Add extra test to verify button is calling the reducer
    delete app file that used connect as I was keeping it for 'old school reference' but decided against it

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import App from "../src/app";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
@@ -21,5 +21,34 @@ describe("<App />", () => {
         <App />
       </Provider>
     );
+  });
+
+  it("should add post when button clicked", () => {
+    const initialState = {
+      posts: []
+    };
+
+    const testReducerActionFunc = jest.fn();
+
+    const testReducer = (state = initialState, action) => {
+      testReducerActionFunc(action);
+      return state;
+    };
+
+    const store = createStore(testReducer);
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    const addButton = getByText("Add");
+    fireEvent.click(addButton);
+
+    expect(testReducerActionFunc).toHaveBeenCalledWith({
+      type: "ADD_POST",
+      payload: { id: 1, title: "New post" },
+    });
   });
 });
